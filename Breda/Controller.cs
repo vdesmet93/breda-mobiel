73acc0c77eb510b928c85a177e70a3f9a3edd8be
remyoman@gmail.com <remyoman@gmail.com@ad06042f-f7a2-cb27-1d61-ac20111a5d4c>
2011-12-12 13:11:44 +0000

    Created Controller skeleton.

--- Breda/Controller.cs
+++ Breda/Controller.cs
@@ -13,6 +13,52 @@ namespace Controller
 {
     public class Controller
     {
+        private Breda.App bredamobiel;
+        private Model.FileManager fileIO;
 
+        public Controller()
+        {
+
+        }
+
+        public calcSpecifications(POI tohere)
+        {
+
+        }
+
+        public changePOIColor()
+        {
+
+        }
+
+        public generateRoute(POI newloc)
+        {
+
+        }
+
+        public getHelpData()
+        {
+
+        }
+
+        public getInfo(POI info)
+        {
+
+        }
+
+        public getMap()
+        {
+
+        }
+
+        private correctRoute()
+        {
+
+        }
+
+        private detectPOI()
+        {
+
+        }
     }
 }
