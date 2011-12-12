eefd8718b7c28f4f7ea80db5c36d67179055a895
vdesmet93 <vdesmet93@ad06042f-f7a2-cb27-1d61-ac20111a5d4c>
2011-12-12 18:43:44 +0000

    FileManager wat uitgebreid, poging gedaan tot database aanmaken ergens vanuit server nogwat.

--- trunk/Breda/FileManager.cs
+++ trunk/Breda/FileManager.cs
@@ -14,5 +14,34 @@ namespace Model
     public class FileManager
     {
 
+        public FileManager()
+        {
+
+        }
+
+        /// <summary>
+        /// Saves the specified data at the specified location.
+        /// </summary>
+        /// <param name="location">The location where the data will be stored.</param>
+        /// <param name="data">The data which will be stored.</param>
+        /// <returns>true if data is saved successfully, otherwise false</returns>
+        public bool Save(string location, string data)
+        {
+
+            // failed to save data
+            return false;
+        }
+        /// <summary>
+        /// Loads data from the specified location.
+        /// </summary>
+        /// <param name="location">The location where the data is stored.</param>
+        /// <returns>The loaded data</returns>
+        public string Load(string location)
+        {
+
+            // no data
+            return "";
+        }
+    
     }
 }
