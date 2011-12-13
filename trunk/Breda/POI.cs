8aa454777ec300fd81ceabd78e529cd739dd98fc
vdesmet93 <vdesmet93@ad06042f-f7a2-cb27-1d61-ac20111a5d4c>
2011-12-13 11:44:55 +0000

    Documentatie toegevoegd xD

--- trunk/Breda/POI.cs
+++ trunk/Breda/POI.cs
@@ -11,11 +11,18 @@ namespace View
         public Double breedteGraad;
         public String info;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="POI"/> class.
+        /// </summary>
+        /// <param name="l">The l.</param>
+        /// <param name="b">The b.</param>
+        /// <param name="i">The i.</param>
         public POI(Double l, Double b, String i)
         {
             this.lengteGraad = l;
             this.breedteGraad = b;
             this.info = i;
+            
         }
     }
 }
