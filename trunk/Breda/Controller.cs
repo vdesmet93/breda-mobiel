7713de6067aba2e858161c976d5465b5381c7494
vdesmet93 <vdesmet93@ad06042f-f7a2-cb27-1d61-ac20111a5d4c>
2011-12-13 19:06:50 +0000

    Database toegevoegd, staat nu nog in klasse Controller maar dat kan eenvoudig worden veranderd. Morgen vul ik de database wel zodat er meer dan 3 waardes in staan, maar het lastigste is gedaan.

--- trunk/Breda/Controller.cs
+++ trunk/Breda/Controller.cs
@@ -10,6 +10,9 @@ using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using View;
 using System.Device.Location;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
 
 namespace Controller
 {
@@ -19,11 +22,29 @@ namespace Controller
         GeoCoordinateWatcher watcher;
         private Breda.App bredamobiel;
         private Model.FileManager fileIO;
+        private Database db;
         //private POI[] POIs = new POI[] { new POI(51.3, 19.2, ""), new POI(51.3, 19.2, ""), new POI(51.3, 19.2, "") };
 
         public bool hasLocation { get; private set; }
         public double latitude { get; private set; }
         public double longtitude { get; private set; }
+
+        private ObservableCollection<DatabaseTable> _DatabaseTables;
+        public ObservableCollection<DatabaseTable> DatabaseTables
+        {
+            get
+            {
+                return _DatabaseTables;
+            }
+            set
+            {
+                if (_DatabaseTables != value)
+                {
+                    _DatabaseTables = value;
+                    NotifyPropertyChanged("DatabaseTable");
+                }
+            }
+        }
 
         /// <summary>
         /// Initializes a new instance of the <see cref="Controller"/> class.
@@ -31,7 +52,8 @@ namespace Controller
         /// <param name="breda">The breda.</param>
         public Controller(Breda.App breda)
         {
-            
+            _DatabaseTables = new ObservableCollection<DatabaseTable>();
+            DatabaseTables = new ObservableCollection<DatabaseTable>();
             bredamobiel = breda;
             fileIO = new Model.FileManager();
 
@@ -44,6 +66,51 @@ namespace Controller
             watcher.PositionChanged += this.watcher_PositionChanged;
             watcher.StatusChanged += this.watcher_StatusChanged;
             watcher.Start();
+
+            // Create the database if it does not exist.
+            db = new Database();
+
+            //Create the database if necessarily
+            if (!db.DatabaseExists())
+            {
+                db.CreateDatabase();
+                fillDatabase();
+            }
+            
+            
+
+
+            
+
+
+            // read data?
+            var DB = from DatabaseTable databasetable in db.databaseTables select databasetable;
+            // Execute query and place results into a collection.
+            DatabaseTables = new ObservableCollection<DatabaseTable>(DB);
+
+            
+            String text = String.Format("{0}, {1}, {2}, {3} \n", DatabaseTables[0].Nummer, DatabaseTables[0].Longitude, DatabaseTables[0].Latitude, DatabaseTables[0].Naam);
+            text += String.Format("{0}, {1}, {2}, {3} \n", DatabaseTables[1].Nummer, DatabaseTables[1].Longitude, DatabaseTables[1].Latitude, DatabaseTables[1].Naam);
+            text += String.Format("{0}, {1}, {2}, {3}", DatabaseTables[2].Nummer, DatabaseTables[2].Longitude, DatabaseTables[2].Latitude, DatabaseTables[2].Naam);
+
+        }
+
+        private void fillDatabase()
+        {
+
+            DatabaseTable item = new DatabaseTable { Nummer = 1, Latitude = 51.5938D, Longitude = 4.77963D, Naam = "VVV Breda" };
+            DatabaseTables.Add(item);
+            db.databaseTables.InsertOnSubmit(item);
+
+            item = new DatabaseTable { Nummer = 2, Latitude = 51.59307D, Longitude = 4.77969D, Naam = "Liefdeszuster" };
+            DatabaseTables.Add(item);
+            db.databaseTables.InsertOnSubmit(item);
+
+            item = new DatabaseTable { Nummer = 3, Latitude = 51.59250D, Longitude = 4.77969D, Naam = "Valkenberg" };
+            DatabaseTables.Add(item);
+            db.databaseTables.InsertOnSubmit(item);
+
+            db.SubmitChanges();
         }
 
         /// <summary>
@@ -150,5 +217,20 @@ namespace Controller
         {
             return fileIO;
         }
+
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Used to notify Silverlight that a property has changed.
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+        #endregion
     }
 }
---
+++ trunk/Breda/DatabaseTable.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.ComponentModel;
+using System.Data.Linq;
+using System.Data.Linq.Mapping;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+
+namespace View
+{
+    [Table]
+    public class DatabaseTable : INotifyPropertyChanged, INotifyPropertyChanging
+    {
+        // Define ID: private field, public property and database column.
+        private int _Nummer;
+
+        [Column(IsPrimaryKey = true, IsDbGenerated = true, DbType = "INT NOT NULL Identity", CanBeNull = false, AutoSync = AutoSync.OnInsert)]
+        public int Nummer
+        {
+            get
+            {
+                return _Nummer;
+            }
+            set
+            {
+                if (_Nummer != value)
+                {
+                    NotifyPropertyChanging("Nummer");
+                    _Nummer = value;
+                    NotifyPropertyChanged("Nummer");
+                }
+            }
+        }
+
+        private double _Latitude;
+
+        [Column(CanBeNull = false)]
+        public double Latitude
+        {
+            get
+            {
+                return _Latitude;
+            }
+            set
+            {
+                if (_Latitude != value)
+                {
+                    NotifyPropertyChanging("Latitude");
+                    _Latitude = value;
+                    NotifyPropertyChanged("Latitude");
+                }
+            }
+        }
+
+        private double _Longitude;
+
+        [Column(CanBeNull = false)]
+        public double Longitude
+        {
+            get
+            {
+                return _Longitude;
+            }
+            set
+            {
+                if (_Longitude != value)
+                {
+                    NotifyPropertyChanging("Longitude");
+                    _Longitude = value;
+                    NotifyPropertyChanged("Longitude");
+                }
+            }
+        }
+        // Define item name: private field, public property and database column.
+        private string _Naam;
+
+        [Column]
+        public string Naam
+        {
+            get
+            {
+                return _Naam;
+            }
+            set
+            {
+                if (_Naam != value)
+                {
+                    NotifyPropertyChanging("Naam");
+                    _Naam = value;
+                    NotifyPropertyChanged("Naam");
+                }
+            }
+        }
+
+        // define beschrijving
+        private string _Beschrijving;
+
+        [Column]
+        public string Beschrijving
+        {
+            get
+            {
+                return _Beschrijving;
+            }
+            set
+            {
+                if (_Beschrijving != value)
+                {
+                    NotifyPropertyChanging("Beschrijving");
+                    _Beschrijving = value;
+                    NotifyPropertyChanged("Beschrijving");
+                }
+            }
+        }
+
+        // define Uitleg
+        private string _Uitleg;
+
+        [Column]
+        public string Uitleg
+        {
+            get
+            {
+                return _Uitleg;
+            }
+            set
+            {
+                if (_Uitleg != value)
+                {
+                    NotifyPropertyChanging("Uitleg");
+                    _Uitleg = value;
+                    NotifyPropertyChanged("Uitleg");
+                }
+            }
+        }
+
+       #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // Used to notify the page that a data context property changed
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanging Members
+
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        // Used to notify the data context that a data context property is about to change
+        private void NotifyPropertyChanging(string propertyName)
+        {
+            if (PropertyChanging != null)
+            {
+                PropertyChanging(this, new PropertyChangingEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+
+        
+    }
+
+}
