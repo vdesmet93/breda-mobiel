5bf5ecd56813f29b39ede6e148df901a9a7814f3
vdesmet93 <vdesmet93@ad06042f-f7a2-cb27-1d61-ac20111a5d4c>
2011-12-19 10:39:21 +0000

    FileManager kan nu opslaan, laden, verwijderen en Laden en verwijderen tegelijk.

--- trunk/Breda/Controller.cs
+++ trunk/Breda/Controller.cs
@@ -1,8 +1,18 @@
 ﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Device.Location;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
-using System.Device.Location;
 using System.Linq;
+using Microsoft.Phone.Controls.Maps;
 using View;
 
 namespace Controller
@@ -13,16 +23,12 @@ namespace Controller
         private Breda.App bredamobiel;
         private Model.FileManager fileIO;
         private Database db;
-        private bool hasLocation { get; set; }
-        private double latitude { get; set; }
-        private double longtitude { get; set; }
+        private  bool hasLocation { get; set; }
+        private  double latitude { get; set; }
+        private  double longtitude { get; set; }
         public event OnLocationChanged LocationChanged;
-        private ObservableCollection<DatabaseTable> _DatabaseTables;
-        /// <summary>This delegate is called when the location has changed.</summary>
-        /// <param name="l">The changed lcoation.</param>
         public delegate void OnLocationChanged(GeoCoordinate l);
-        /// <summary>Gets or sets the database tables.</summary>
-        /// <value>The database tables.</value>
+        private ObservableCollection<DatabaseTable> _DatabaseTables;
         public ObservableCollection<DatabaseTable> DatabaseTables
         {
             get
@@ -39,8 +45,9 @@ namespace Controller
             }
         }
 
-        /// <summary>Initializes a new instance of the <see cref="Controller"/> class.</summary>
-        /// <remarks>The constructor intializes the database, GPS and the FileManager used for FileIO</remarks>
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Controller"/> class.
+        /// </summary>
         /// <param name="breda">The breda app.</param>
         public Controller(Breda.App breda)
         {
@@ -69,75 +76,111 @@ namespace Controller
             var DB = from DatabaseTable databasetable in db.databaseTables select databasetable;
             // Execute query and place results into a collection.
             DatabaseTables = new ObservableCollection<DatabaseTable>(DB);
+            
             String text = String.Format("{0}, {1}, {2}, {3} \n", DatabaseTables[0].Nummer, DatabaseTables[0].Longitude, DatabaseTables[0].Latitude, DatabaseTables[0].Naam);
             text += String.Format("{0}, {1}, {2}, {3} \n", DatabaseTables[1].Nummer, DatabaseTables[1].Longitude, DatabaseTables[1].Latitude, DatabaseTables[1].Naam);
             text += String.Format("{0}, {1}, {2}, {3}", DatabaseTables[2].Nummer, DatabaseTables[2].Longitude, DatabaseTables[2].Latitude, DatabaseTables[2].Naam);
+
         }
 
-        /// <summary>Fills the database with all the POI's used in the application.</summary>
+        /// <summary>
+        /// Fills the database.
+        /// </summary>
         private void fillDatabase()
         {
+
             DatabaseTable item = new DatabaseTable { Nummer = 1, Latitude = 51.5938D, Longitude = 4.77963D, Naam = "VVV Breda" };
             db.databaseTables.InsertOnSubmit(item);
+
             item = new DatabaseTable { Nummer = 2, Latitude = 51.59327833333333D, Longitude = 4.779388333333333D, Naam = "Liefdeszuster" };
             db.databaseTables.InsertOnSubmit(item);
+
             item = new DatabaseTable { Nummer = 3, Latitude = 51.59250D, Longitude = 4.779695D, Naam = "Nassau Baronie Monument" };
             db.databaseTables.InsertOnSubmit(item);
+
             item = new DatabaseTable { Nummer = 4, Latitude = 51.59327833333333D, Longitude = 4.779388333333333D, Naam = "Liefdeszuster" };
             db.databaseTables.InsertOnSubmit(item);
+
             item = new DatabaseTable { Nummer = 5, Latitude = 51.59283333333333D, Longitude = 4.7784716666666665D, Naam = "The Light house" };
             db.databaseTables.InsertOnSubmit(item);
+
             item = new DatabaseTable { Nummer = 6, Latitude = 51.59061166666667D, Longitude = 4.776166666666667D, Naam = "Kasteel van Breda" };
             db.databaseTables.InsertOnSubmit(item);
+
             item = new DatabaseTable { Nummer = 7, Latitude = 51.589695D, Longitude = 4.776138333333334D, Naam = "Stadhouderspoort" };
             db.databaseTables.InsertOnSubmit(item);
+
             item = new DatabaseTable { Nummer = 8, Latitude = 51.590028333333336D, Longitude = 4.774361666666667D, Naam = "Huis van Brecht" };
             db.databaseTables.InsertOnSubmit(item);
+
             item = new DatabaseTable { Nummer = 9, Latitude = 51.590195D, Longitude = 4.773445D, Naam = "Spanjaardsgat" };
             db.databaseTables.InsertOnSubmit(item);
+
             item = new DatabaseTable { Nummer = 10, Latitude = 51.58983333333333D, Longitude = 4.773333333333333D, Naam = "Vismarkt" };
             db.databaseTables.InsertOnSubmit(item);
+
             item = new DatabaseTable { Nummer = 11, Latitude = 51.58936166666667D, Longitude = 4.774445D, Naam = "Havermarkt" };
             db.databaseTables.InsertOnSubmit(item);
+
             item = new DatabaseTable { Nummer = 12, Latitude = 51.58883333333333D, Longitude = 4.7752783333333335D, Naam = "Grote Kerk" };
             db.databaseTables.InsertOnSubmit(item);
+
             item = new DatabaseTable { Nummer = 13, Latitude = 51.588195D, Longitude = 4.7751383333333335D, Naam = "Het Poortje" };
             db.databaseTables.InsertOnSubmit(item);
+
             item = new DatabaseTable { Nummer = 14, Latitude = 51.58708333333333D, Longitude = 4.77575D, Naam = "Ridderstraat" };
             db.databaseTables.InsertOnSubmit(item);
+
             item = new DatabaseTable { Nummer = 15, Latitude = 51.58741666666667D, Longitude = 4.776555D, Naam = "Grote Markt" };
             db.databaseTables.InsertOnSubmit(item);
+
             item = new DatabaseTable { Nummer = 16, Latitude = 51.588028333333334D, Longitude = 4.7763333333333335D, Naam = "Bevrijdingsmonument" };
             db.databaseTables.InsertOnSubmit(item);
+
             item = new DatabaseTable { Nummer = 17, Latitude = 51.58875D, Longitude = 4.776111666666667D, Naam = "StadHuis" };
             db.databaseTables.InsertOnSubmit(item);
+
             item = new DatabaseTable { Nummer = 18, Latitude = 51.58763833333333D, Longitude = 4.77725D, Naam = "Antonius van Paduakerk" };
             db.databaseTables.InsertOnSubmit(item);
+
             item = new DatabaseTable { Nummer = 19, Latitude = 51.588D, Longitude = 4.778945D, Naam = "Bibliotheek" };
             db.databaseTables.InsertOnSubmit(item);
+
             item = new DatabaseTable { Nummer = 20, Latitude = 51.58772166666667D, Longitude = 4.781028333333333D, Naam = "Kloosterkazerne" };
             db.databaseTables.InsertOnSubmit(item);
+
             item = new DatabaseTable { Nummer = 21, Latitude = 51.58775D, Longitude = 4.782D, Naam = "Chasse theater" };
             db.databaseTables.InsertOnSubmit(item);
+
             item = new DatabaseTable { Nummer = 22, Latitude = 51.58775D, Longitude = 4.78125D, Naam = "Binding van Isaac" };
             db.databaseTables.InsertOnSubmit(item);
+
             item = new DatabaseTable { Nummer = 23, Latitude = 51.589666666666666D, Longitude = 4.781D, Naam = "Beyerd" };
             db.databaseTables.InsertOnSubmit(item);
+
             item = new DatabaseTable { Nummer = 24, Latitude = 51.589555D, Longitude = 4.78D, Naam = "Gasthuispoort" };
             db.databaseTables.InsertOnSubmit(item);
+
             item = new DatabaseTable { Nummer = 25, Latitude = 51.58911166666667D, Longitude = 4.777945D, Naam = "Willem Merkxtuin" };
             db.databaseTables.InsertOnSubmit(item);
+
             item = new DatabaseTable { Nummer = 26, Latitude = 51.589695D, Longitude = 4.778361666666667D, Naam = "Begijnenhof" };
             db.databaseTables.InsertOnSubmit(item);
+
             item = new DatabaseTable { Nummer = 27, Latitude = 51.5895D, Longitude = 4.77625D, Naam = "Einde stadswandeling" };
             db.databaseTables.InsertOnSubmit(item);
+
+
             db.SubmitChanges();
+
             var DB = from DatabaseTable databasetable in db.databaseTables select databasetable;
             // Execute query and place results into a collection.
             DatabaseTables = new ObservableCollection<DatabaseTable>(DB);
         }
 
-        /// <summary>Handles the StatusChanged event of the watcher control.</summary>
+        /// <summary>
+        /// Handles the StatusChanged event of the watcher control.
+        /// </summary>
         /// <param name="sender">The source of the event.</param>
         /// <param name="e">The <see cref="System.Device.Location.GeoPositionStatusChangedEventArgs"/> instance containing the event data.</param>
         private void watcher_StatusChanged(object sender, GeoPositionStatusChangedEventArgs e)
@@ -155,7 +198,9 @@ namespace Controller
             }
         }
 
-        /// <summary>Handles the PositionChanged event of the watcher control.</summary>
+        /// <summary>
+        /// Handles the PositionChanged event of the watcher control.
+        /// </summary>
         /// <param name="sender">The source of the event.</param>
         /// <param name="e">The <see cref="System.Device.Location.GeoPositionChangedEventArgs&lt;System.Device.Location.GeoCoordinate&gt;"/> instance containing the event data.</param>
         private void watcher_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
@@ -164,54 +209,83 @@ namespace Controller
             var epl = e.Position.Location;
             latitude = epl.Latitude;
             longtitude = epl.Longitude;
-            if (LocationChanged != null) LocationChanged(getLocation());
+            if(LocationChanged != null) LocationChanged(getLocation());
         }
-        
-        /// <summary>Calcs the POI to navigate to..</summary>
-        /// <param name="tohere">The POI to go to.</param>
+        /// <summary>
+        /// Calcs the specifications.
+        /// </summary>
+        /// <param name="tohere">The POI to g.</param>
         public void calcSpecifications(POI tohere)
         {
-
+            
         }
 
-        /// <summary>Changes the color of the POI after the user has passed the POI while using the application.</summary>
+        /// <summary>
+        /// Changes the color of the POI after the user has passed the POI while using the application.
+        /// </summary>
         public void changePOIColor()
         {
 
         }
 
-        /// <summary>Calculates the route to the selected POI.</summary>
-        /// <remarks>Will not be implemented in this version of the application.</remarks>
-        /// <param name="newloc">The new location to navigate to.</param>
+        /// <summary>
+        /// Generates the route.
+        /// </summary>
+        /// <param name="newloc">The newloc.</param>
         public void generateRoute(POI newloc)
         {
-
+            
         }
 
-        /// <summary>Gets the extra information about a POI.</summary>
-        /// <param name="info">The POI.</param>
-        /// <returns>The extra information as a string.</returns>
+        /// <summary>
+        /// Gets the help data.
+        /// </summary>
+        /// <returns></returns>
+        public string getHelpData()
+        {
+            return "";
+        }
+
+        /// <summary>
+        /// Gets the info.
+        /// </summary>
+        /// <param name="info">The info.</param>
+        /// <returns></returns>
         public string getInfo(POI info)
         {
             return info.informatie;
         }
 
-        /// <summary>Checks if the user is on the route to navigate.</summary>
-        /// <returns>Returns true if the user is on the route, otherwise false</returns>
+        /// <summary>
+        /// Gets the map.
+        /// </summary>
+        /// <returns></returns>
+        public Object getMap()
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Corrects the route.
+        /// </summary>
+        /// <returns></returns>
         private Boolean correctRoute()
         {
             return false;
         }
 
-        /// <summary>Checks if the suer has passes a POI.</summary>
-        /// <returns>Returns true if the user passed a POI, otherwise false.</returns>
-        private Boolean detectPOI()
+        /// <summary>
+        /// Detects the POI.
+        /// </summary>
+        private void detectPOI()
         {
-            return false;
+
         }
 
-        /// <summary>Retrieves the current or last known location.</summary>
-        /// <returns>The Coordinates.</returns>
+        /// <summary>
+        /// Gets the location.
+        /// </summary>
+        /// <returns></returns>
         public GeoCoordinate getLocation()
         {
             GeoCoordinate l = new GeoCoordinate();
@@ -221,9 +295,11 @@ namespace Controller
 
         }
 
-        /// <summary>Returns the FileManager which handles FileIO.</summary>
-        /// <returns>The FIleManager.</returns>
-        public Model.FileManager getFileIO()
+        /// <summary>
+        /// Gets the file IO.
+        /// </summary>
+        /// <returns></returns>
+        internal object getFileIO()
         {
             return fileIO;
         }
--- trunk/Breda/FileManager.cs
+++ trunk/Breda/FileManager.cs
@@ -8,6 +8,9 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
+using System.IO.IsolatedStorage;
+using System.IO;
+using System.Text;
 
 namespace Model
 {
@@ -21,13 +24,29 @@ namespace Model
         }
 
         /// <summary>
-        /// Saves the specified data at the specified location.
+        /// /// Saves the specified data at the specified location.
         /// </summary>
-        /// <param name="location">The location where the data will be stored.</param>
+        /// <param name="fileName">Name of the file.</param>
         /// <param name="data">The data which will be stored.</param>
-        /// <returns>true if data is saved successfully, otherwise false</returns>
-        public bool Save(string location, string data)
+        /// <returns>true if data is saved, false otherwise</returns>
+        public bool Save(string fileName, string data)
         {
+            try
+            {
+                var appStorage = IsolatedStorageFile.GetUserStoreForApplication();
+                if (!appStorage.FileExists(fileName))
+                {
+                    using (var file = appStorage.CreateFile(fileName))
+                    {
+                        using (var writer = new StreamWriter(file))
+                        {
+                            writer.WriteLine(data);
+                        }
+                    }
+                    return true;
+                }
+            }
+            catch {}
             // failed to save data
             return false;
         }
@@ -38,8 +57,63 @@ namespace Model
         /// <returns>The loaded data</returns>
         public string Load(string location)
         {
+            var appStorage = IsolatedStorageFile.GetUserStoreForApplication();
+            if (appStorage.FileExists(location))
+            {
+                using (IsolatedStorageFileStream stream = appStorage.OpenFile(location, FileMode.Open))
+                {
+                    long length = stream.Length;
+
+                    byte[] decoded = new byte[length];
+                    stream.Read(decoded, 0, (int)length);
+                    return Encoding.UTF8.GetString(decoded, 0, (int)length);
+                }
+            }
             // no data
             return "";
+        }
+
+        /// <summary>
+        /// Loads the file and deletes it.
+        /// </summary>
+        /// <param name="location">The location of the file.</param>
+        /// <returns>The content of the file</returns>
+        public string LoadAndDelete(string location)
+        {
+            var appStorage = IsolatedStorageFile.GetUserStoreForApplication();
+            string content = String.Empty;
+
+            if (appStorage.FileExists(location))
+            {
+                using (IsolatedStorageFileStream stream = appStorage.OpenFile(location, FileMode.Open))
+                {
+                    long length = stream.Length;
+
+                    byte[] decoded = new byte[length];
+                    stream.Read(decoded, 0, (int)length);
+                    content =  Encoding.UTF8.GetString(decoded, 0, (int)length);
+                }
+                appStorage.DeleteFile(location);
+            }
+            // no data
+            return content;
+        }
+
+        /// <summary>
+        /// Deletes the file at the specified location.
+        /// </summary>
+        /// <param name="location">The location of the file.</param>
+        /// <returns>true if file is deleted</returns>
+        public bool Delete(string location)
+        {
+            try
+            {
+                var appStorage = IsolatedStorageFile.GetUserStoreForApplication();
+                appStorage.DeleteFile(location);
+                return true;
+            }
+            catch { }
+            return false;
         }
     
     }
