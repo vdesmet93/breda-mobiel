7a1cad42c3a22f391c45a1c4535b994c5caee68f
vdesmet93 <vdesmet93@ad06042f-f7a2-cb27-1d61-ac20111a5d4c>
2011-12-21 10:55:35 +0000

    isUitgaan toegevoegd, meer-functie bij POI werkt.

--- trunk/Breda/POIinfoScreen.xaml.cs
+++ trunk/Breda/POIinfoScreen.xaml.cs
@@ -3,32 +3,41 @@ using System.Net;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using Microsoft.Phone.Tasks;
+using System.Windows.Navigation;
 
 namespace View
 {
     public partial class POIinfoScreen : ChildWindow
     {
+        private String naam;
         public string HostName { get; set; }
 
-        public POIinfoScreen(String info)
+        public POIinfoScreen(string naam, String info)
         {
+            if (info == null) info = String.Empty;
             InitializeComponent();
-            Textbox1.Text = info;
+            this.naam = naam;
+            Textbox.Text = info;
         }
 
         private void closeButton_Click(object sender, RoutedEventArgs e)
         {
+            //this.Visibility = Visibility.Collapsed;
             this.Close();
         }
 
         private void ChildWindow_Closed(object sender, EventArgs e)
         {
-            this.Close();
+
         }
 
         private void Meerbutton_Click(object sender, RoutedEventArgs e)
         {
-
+            WebBrowserTask task = new WebBrowserTask();
+            task.URL = "http://lmbtfy.com/?q=" + naam;
+            task.Show();
+            
         }
     }
 }
