b31a28c157587f56ed099b4e79c6da22f9b718c8
remyoman@gmail.com <remyoman@gmail.com@ad06042f-f7a2-cb27-1d61-ac20111a5d4c>
2011-12-23 11:53:21 +0000

    Updated route visual updating. Currently erases main route, looking into it.

--- trunk/Breda/POI.cs
+++ trunk/Breda/POI.cs
@@ -19,11 +19,8 @@ namespace View
         public int nummer { get; private set; }
         public Pushpin pushpin { get; private set; }
         public MapView m { get; private set; }
-        /// <summary>Initializes a new instance of the <see cref="POI"/> class.</summary>
-        /// <param name="l">The longtitude.</param>
-        /// <param name="b">The latitude.</param>
-        /// <param name="i">The exra information.</param>
-        /// <param name="n">The number.</param>
+        public double latitude { get; private set; }
+        public double longitude { get; private set; }
         
         public POI(GeoCoordinate g, MapView m, string naam, bool isUitgaan, string info, int nummer,int foto)
         {
@@ -33,6 +30,8 @@ namespace View
             this.naam = naam;
             this.isUitgaan = isUitgaan;
             this.foto = foto;
+            this.latitude = g.Latitude;
+            this.longitude = g.Longitude;
             intializePushpin(g);
             informatie = info ?? "";
             if (String.IsNullOrEmpty(informatie))
@@ -57,22 +56,26 @@ namespace View
             else
             {
                 if (m.themeColor == Colors.Red && isUitgaan)
-                { a = m.themeColor;}
+                {
+                    a = m.themeColor;
+                }
                 else if (m.themeColor == Colors.Blue && !isUitgaan)
-                { a = m.themeColor;  }
+                {
+                    a = m.themeColor;
+                }
                 else
-                { a = Color.FromArgb(250, 150, 150, 150); }   
+                {
+                    a = Color.FromArgb(250, 150, 150, 150);
+                }
             }
             b = a;
             if (isBezocht)
             {
                 b.R -= 60; b.G -= 60; b.B -= 60;
             }
-
             pushpin.Content = new Ellipse()
             {
-                Fill = new SolidColorBrush(a),
-                Stroke = new SolidColorBrush(b),
+                Fill = new SolidColorBrush(b),
                 StrokeThickness = 5,
                 Opacity = .8,
                 Height = 20,
